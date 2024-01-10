c0a93fdf9aa2f6c385e5fe2997d2b123060bd862
jaraim <117310975+jaraim@users.noreply.github.com>
2024-01-10 20:23:04 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'vps.jatk.eu.org'
+      url.hostname = 'app.104.194.74.122.nip.io'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
